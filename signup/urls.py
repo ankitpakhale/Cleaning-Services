d240a6b3e6bef5412d2c9e029046b308b265f751
Ankit Pakhale <akp3067@gmail.com>
2021-09-29 19:27:02 +0530

    login done

--- signup/urls.py
+++ signup/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from .views import userSignUp, userLogin
-from app1.views import home
 
 urlpatterns = [
     path('', userSignUp, name='SIGNUP'),
     
     path('login/', userLogin, name='LOGIN'),
-
-    path('home/', home, name='HOME'),
 
 ]
--- signup/views.py
+++ signup/views.py
@@ -30,20 +30,33 @@ def userSignUp(request):
         return redirect('HOME')
     return render(request, 'signup.html')
 
+# def userLogin(request):
+#     if request.POST:
+#         Email = request.POST['email']
+#         print(Email)
+#         Password = request.POST['password']
+#         print(Password)
+#         log = signUp()
+#         log.email = Email
+#         log.password = Password
+#         log.save()
+#         # messages.success(request, 'Done')
+#         # return HttpResponseRedirect('http://127.0.0.1:8000/signup/login/')
+#         return redirect('HOME')
+#     return render(request, 'login.html')
 
 def userLogin(request):
     if request.POST:
-        Email = request.POST['email']
-        print(Email)
-        Password = request.POST['password']
-        print(Password)
-
-        log = signUp()
-
-        log.email = Email
-        log.password = Password
-        log.save()
-        # messages.success(request, 'Done')
-        # return HttpResponseRedirect('http://127.0.0.1:8000/signup/login/')
-        return redirect('HOME')
+        em = request.POST.get('email')
+        pass1 = request.POST.get('password')
+        try:
+            check = signUp.objects.get(email = em)
+            if check.password == pass1:
+                return redirect('HOME')
+            else:
+                msg = "Wrong Password"
+            return render(request, 'login.html', {'msg' : msg})
+        except:
+            msg = "Wrong Email ID"
+            return render(request, 'login.html', {'msg' : msg})
     return render(request, 'login.html')
