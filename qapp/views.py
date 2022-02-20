89650799dc60a52ee1577420c519f35d1929d8b5
ankitpakhale <akp3067@gmail.com>
2022-02-20 13:44:52 +0530

    qapp query commit

--- qapp/views.py
+++ qapp/views.py
@@ -8,20 +8,24 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def query(request):
+    # 1st query
     # b = Blog(name = "ank", tagline = "this is just a demo tagline")
     # b.save()
 
+    # 2nd query  
     # doubted query 1
     # joe = Author.objects.create(name = "Joe")
     # entry.authors.add(joe)
     
   
+    # 3rd query
     # doubted query 2
     entry = Entry.objects.get(pk=1)
     cheese_blog = Blog.objects.get(name="ank")
     entry.blog = cheese_blog
     entry.save()
     
+    # 4th query
     # john = Author.objects.create(name = "John")
     # lucky = Author.objects.create(name="Lucky")
     # harry = Author.objects.create(name="harry")
@@ -29,5 +33,10 @@ def query(request):
     # entry.authors.add(john, lucky, harry, ank) 
     # above line will add multiple Author in Author field in Entry model
     # Entry --> Author --> above line
+    
+    # 5th query
+    
+    
+    
     
     return HttpResponse("working properly")
