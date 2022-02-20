ed077de7f7292565aea186113d69fb638a153589
ankitpakhale <akp3067@gmail.com>
2022-02-20 12:46:16 +0530

    qapp query commit

--- qapp/views.py
+++ qapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import *
+from .models import Blog, Entry
 
 # Create your views here.
 
@@ -11,6 +11,14 @@ def query(request):
     # b = Blog(name = "ank", tagline = "this is just a demo tagline")
     # b.save()
     
-    joe = Author.objects.create(name = "Joe")
-    joe.save()
+    entry = Entry.objects.get(pk=1)
+    cheese_blog = Blog.objects.get(name="ank")
+    entry.blog = cheese_blog
+    entry.save()
+    
+    # joe = Author.objects.create(name = "Joe")
+    # entry.authors.add(joe)
+    
+    # john = Author.objects.create(name = "John")
+    
     return HttpResponse("working properly")
